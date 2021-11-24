e5a91aebe9544b995ffe9f35893297a24e2b1da1
Jay <jasonsaayman@gmail.com>
2021-11-24 16:46:26 +0200

    Fixing code style and remove scripts section for now

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const db = require('./dist/Connection');
-const Http = require('./dist/Http');
-const Controller = require('./dist/Controller');
+const db = require('./dist/Connection')
+const Http = require('./dist/Http')
+const Controller = require('./dist/Controller')
 
 module.exports = {
   db,
   Http,
-  Controller
-};
+  Controller,
+}
