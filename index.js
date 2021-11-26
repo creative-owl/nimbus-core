ada5842a4ebf140004d0d3b78b2020937f664096
Jay <jasonsaayman@gmail.com>
2021-11-26 11:10:17 +0200

    Exposed Jwt handler in export

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const db = require('./dist/Connection')
 const Http = require('./dist/Http')
 const Controller = require('./dist/Controller')
+const JwtHandler = require('./dist/Auth/JwtHandler')
 
 module.exports = {
   db,
   Http,
   Controller,
+  JwtHandler,
 }
