b267937b3efa69313a518e0866e701ed5a91a5f4
Jay <jasonsaayman@gmail.com>
2021-11-26 11:28:46 +0200

    Expose models

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const db = require('./dist/Connection')
 const Http = require('./dist/Http')
+const Roles = require('./dist/Models/Roles')
 const Controller = require('./dist/Controller')
 const JwtHandler = require('./dist/Auth/JwtHandler')
+const Permissions = require('./dist/Models/Permissions')
+const RolesPermissions = require('./dist/Models/RolesPermissions')
 
 module.exports = {
   db,
   Http,
+  Roles,
   Controller,
   JwtHandler,
+  Permissions,
+  RolesPermissions,
 }
